188c02dd94059d2462d544b2be08078e78065925
Andre Zimpel <andre@unknownbasics.com>
2021-07-22 07:28:28 +0200

    updated colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
         name: `ONCUE`,
         short_name: `ONCUE`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#cc5c5b`,
+        theme_color: `#cc5c5b`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
